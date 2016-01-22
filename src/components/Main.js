66f130ee5ba8a3054f185e4ba7655ec2d86f4d0f
Richard <richard.anaya@gmail.com>
2016-01-21 23:01:33 -0600

    using refs

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,16 +3,21 @@ import {listen,publish,component} from "../core/utils"
 import {Card, CardTitle, CardText, CardActions, Button} from "react-mdl";
 import fetch from "isomorphic-fetch"
 
-export default component((props) => {
+listen("save",function *(data){
+  window.__firebase__.set(data);
+})
+
+export default component((props,refs) => {
   return <div className="page-container">
   <Card shadow={0} className="card-center">
     <CardTitle>Data Card</CardTitle>
     <CardText>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         Aenan convallis.
+        <input ref="imp"></input>
     </CardText>
     <CardActions border>
-        <Button colored>Do Something</Button>
+        <Button onClick={()=>publish("save",refs.imp.value)} colored>save</Button>
     </CardActions>
   </Card>
   </div>;
