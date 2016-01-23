2d48a66e1dd5d976fdda6a8fb15e96722152e5b7
Richard <richard.anaya@gmail.com>
2016-01-23 15:16:35 -0600

    adding create

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import {Router, Route, IndexRoute } from "react-router";
 
 import App from "./core/app";
 import Main from "./view/main.component";
+import Create from "./view/create.component";
 
 /**
  * The React Router 1.0 routes for both the server and the client.
@@ -11,6 +12,7 @@ module.exports = (
 	<Router>
 			<Route path="/" component={App}>
 				<IndexRoute component={Main} />
+				<Route path="/create" component={Create} />
 			</Route>
 	</Router>
 );
